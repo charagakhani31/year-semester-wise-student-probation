e50195d643f55b4e75ece782119df088906d17b4
charagakhani31 <charagakhani31@gmail.com>
2026-07-05 02:34:44 +0500

    ProbationRoutes.js

--- ProbationRoutes.js
+++ ProbationRoutes.js
@@ -3,14 +3,14 @@ const router = express.Router();
 import { SQL } from "../utils/db.js";
 
 router.get('/year', async(req, res) => {
-     const year = await SQL `SELECT DISTINCT year from takes;`;
+     const year = await SQL `SELECT DISTINCT year From recap ORDER BY year;`;
      res.status(200).json(year);
 });
 
 router.get('/semester/:year', async(req,res) => {
     const { year } = req.params;
 
-    const semester = await SQL `SELECT DISTINCT semester FROM takes WHERE year = ${year} ORDER BY semester;`;
+    const semester = await SQL `SELECT DISTINCT semester FROM recap WHERE year = ${year} ORDER BY semester;`;
     res.json(semester);
 });
 
@@ -18,26 +18,17 @@ router.get('/probation/:semester/:year', async(req, res) => {
     const { semester } = req.params;
     const { year } = req.params;
 
-    const probation = await SQL `SELECT s.id, s.name, g.year, g.semester, ROUND(g.gpa, 2) AS gpa FROM student s
-                               JOIN (
-                                      SELECT
-                                       t.id, t.year, t.semester,
-                                             SUM(
-                                                  CASE t.grade
-                                                    WHEN 'A'  THEN 3.75 * c.credits
-                                                    WHEN 'A-' THEN 3.5 * c.credits
-                                                    WHEN 'B+' THEN 3.25 * c.credits
-                                                    WHEN 'B'  THEN 3.0 * c.credits
-                                                    WHEN 'B-' THEN 2.75 * c.credits 
-                                                    WHEN 'C+' THEN 2.5 * c.credits
-                                                    WHEN 'C'  THEN 2.0 * c.credits 
-                                                    WHEN 'C-' THEN 1.5 * c.credits
-                                                    WHEN 'F'  THEN 0.0 * c.credits
-                                                 END) / SUM(c.credits) AS gpa FROM takes t
-                                                 JOIN course c ON t.course_id = c.course_id
-                                                 GROUP BY t.id, t.year, t.semester) g ON s.id = g.id
-                                                 WHERE g.year = ${year} AND g.semester = ${semester} AND g.gpa < 1.75
-                                                 ORDER BY s.id;`;
+    const probation = await SQL `SELECT t.regno, s.name, t.year, t.semester, 
+                                ROUND(SUM(t.gpa * t.credits) / SUM(t.credits),2) AS semester_gpa
+                                FROM ( 
+                                SELECT c.regno, c.rid, r.year, r.semester, (co.theory + co.lab) AS credits, g.gpa
+                                FROM cmarks c JOIN recap r ON c.rid = r.rid 
+                                JOIN course co ON r.cid = co.cid 
+                                JOIN grade g ON c.marks BETWEEN g.start AND g.end
+                                WHERE c.hid = 246 AND r.year = ${year} AND r.semester = ${semester}) t
+                                JOIN student s ON t.regno = s.regno
+                                GROUP BY t.regno, s.name, t.year, t.semester
+                                HAVING ROUND( SUM(t.gpa * t.credits) / SUM(t.credits),2 ) < 1.75;`;
     res.json(probation);                       
 })
 
