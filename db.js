13bc2316679d2a91aa874682fe1fc6961bdd0498
charagakhani31 <charagakhani31@gmail.com>
2026-07-17 16:41:00 +0500

    Update db.js

--- db.js
+++ db.js
@@ -1,2 +1,1 @@
-import { neon } from '@neondatabase/serverless';
-export const SQL = neon('postgresql://online:Online.995@ep-icy-heart-a5h7ggwf-pooler.us-east-2.aws.neon.tech/neondb?sslmode=require');
+
